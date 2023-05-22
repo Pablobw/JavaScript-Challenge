a62edeb59a24c6081af2ea00d0886d8edfa4403e
Pablo Barrera <pablobarreraw@gmail.com>
2023-05-22 19:23:20 -0400

    Add files via upload

---
+++ 02_Counter.js
@@ -0,0 +1,7 @@
+// Given an integer n, return a counter function. 
+// This counter function initially returns n and then returns 1 more than the previous 
+// value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+var createCounter = function(n) {
+    return () => n++
+}
