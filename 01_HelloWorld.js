839c2ccab45693852042bac57eca3ceab9eed83a
Pablo Barrera <pablobarreraw@gmail.com>
2023-05-22 17:41:20 -0400

    01_HelloWorld.js

--- 01_HelloWorld.js
+++ 01_HelloWorld.js
@@ -1,3 +1,5 @@
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
 function createHelloWorld() {
     return function() {
       return "Hello World";
